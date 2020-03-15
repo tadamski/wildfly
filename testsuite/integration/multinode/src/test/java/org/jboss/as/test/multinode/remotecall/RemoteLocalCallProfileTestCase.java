fef3ea9312969dc79ffc5d2336932beeb608b2f1
Tomasz Adamski <tadamski@redhat.com>
2020-03-15 13:24:58 +0100

    fixup

--- testsuite/integration/multinode/src/test/java/org/jboss/as/test/multinode/remotecall/RemoteLocalCallProfileTestCase.java
+++ testsuite/integration/multinode/src/test/java/org/jboss/as/test/multinode/remotecall/RemoteLocalCallProfileTestCase.java
@@ -91,9 +91,9 @@ public class RemoteLocalCallProfileTestCase {
             op2.get(OP).set(ADD);
             op2.get(OP_ADDR).add(SUBSYSTEM, "ejb3");
             op2.get(OP_ADDR).add("remoting-profile", "test-profile");
-            op2.get(OP_ADDR).add("remoting-ejb-receiver", "test-receiver");
+            op2.get(OP_ADDR).add("remote-http-connection", "test-connection");
 
-            op2.get("outbound-connection-ref").set("remote-ejb-connection");
+            op2.get("uri").set("http://localhost:8180/wildfly-services");
 
             op2.get(OPERATION_HEADERS).get(ALLOW_RESOURCE_SERVICE_RESTART).set(true);
 
@@ -128,6 +128,7 @@ public class RemoteLocalCallProfileTestCase {
         JavaArchive jar = createJar(ARCHIVE_NAME_CLIENT);
         jar.addClasses(RemoteLocalCallProfileTestCase.class);
         jar.addAsManifestResource("META-INF/jboss-ejb-client-profile.xml", "jboss-ejb-client.xml")
+                .addAsManifestResource("wildfly-config.xml", "wildfly-config.xml")
                 .addAsManifestResource(createPermissionsXmlAsset(createFilePermission("read,write",
                         "jbossas.multinode.client", Arrays.asList("standalone", "data", "ejb-xa-recovery")),
                         createFilePermission("read,write",
